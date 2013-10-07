0fe2e9ad165d01038a92656252f230f6f5013644
Weilin <mzweilin@gmail.com>
2013-10-07 11:22:06 -0400

    Update zhttpto.rs to run on Rust 0.8.

--- ps1/zhttpto.rs
+++ ps1/zhttpto.rs
@@ -2,91 +2,87 @@
 // zhttpto.rs
 //
 // Reference solution for PS1
+// Running on Rust 0.8
+//
+// Note that this code has serious security risks!  You should not run it 
+// on any system with access to sensitive files.
 //
 // Special thanks to Kiet Tran for providing code we incorporated into this.
 // 
-// Note: it would be very unwise to run this server on a machine that is
-// on the Internet and contains any sensitive files!
-//
 // University of Virginia - cs4414 Fall 2013
 // Weilin Xu and David Evans
-// Version 0.1
+// Version 0.2
 
 extern mod extra;
 
-use extra::uv;
-use extra::{net_ip, net_tcp};
-use std::{io, os, str};
+use std::rt::io::*;
+use std::rt::io::net::ip::{SocketAddr, Ipv4Addr};
+use std::io::println;
+use std::cell::Cell;
+use std::task;
+use std::{os, str, io};
 
-static BACKLOG: uint = 5;
-static PORT:    uint = 4414;
+static PORT:    int = 4414;
 static IPV4_LOOPBACK: &'static str = "127.0.0.1";
 static mut visitor_count: uint = 0;
 
-fn new_connection_callback(new_conn :net_tcp::TcpNewConnection, _killch: std::comm::SharedChan<Option<extra::net_tcp::TcpErrData>>)
-{
-    do spawn {
-        let accept_result = extra::net_tcp::accept(new_conn);
-        match accept_result {
-            Err(err) => {
-               println(fmt!("Connection error: %?", err));
-            }
-            Ok(sock) => {
-                let peer_addr: ~str = net_ip::format_addr(&sock.get_peer_addr());
-                println(fmt!("Received connection from: %s", peer_addr));
-                
-                let read_result = net_tcp::read(&sock, 0u);
-                match read_result {
-                    Err(err) => {
-                        println(fmt!("Receive error: %?", err));
-                    }
-                    Ok(bytes) => {
-                        unsafe {
-                            visitor_count += 1;
-                        }
-                        let request_str = str::from_bytes(bytes.slice(0, bytes.len() - 1));
-                        
-                        let path = request_str.split_iter(' ').nth(1).get();
-                        println(fmt!("Request for path: %s", path));
-                        
-                        let file_path = &os::getcwd().push(path.replace("/../", ""));
-                        if !os::path_exists(file_path) || os::path_is_dir(file_path) {
-                            println(fmt!("Request received:\n%s", request_str));
-                            let response: ~str = fmt!(
-                                "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
-                                 <doctype !html><html><head><title>Hello, Rust!</title>
-                                 <style>body { background-color: #111; color: #FFEEAA }
-                                        h1 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm red}
-                                        h2 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm green}
-                                 </style></head>
-                                 <body>
-                                 <h1>Greetings, Krusty!</h1>
-                                 <h2>Visitor count: %u</h2>
-                                 </body></html>\r\n", unsafe{visitor_count});
-
-                            net_tcp::write(&sock, response.as_bytes_with_null_consume());
-                        }
-                        else {
-                            println(fmt!("serve file: %?", file_path));
-                            match io::read_whole_file(file_path) {
-                                Ok(file_data) => {
-                                    sock.write(file_data);
-                                }
-                                Err(err) => {
-                                    println(err);
-                                }
-                            }
-                        }
-                    }
-                };
-            }
-        };
-    };
-}
 
 fn main() {
-    net_tcp::listen(net_ip::v4::parse_addr(IPV4_LOOPBACK), PORT, BACKLOG,
-                    &uv::global_loop::get(),
-                    |_chan| { println(fmt!("Listening on tcp port %u ...", PORT)); },
-                    new_connection_callback);
+    let socket = net::tcp::TcpListener::bind(SocketAddr {ip: Ipv4Addr(127,0,0,1), port: PORT as u16});
+    
+    println(fmt!("Listening on tcp port %d ...", PORT));
+    let mut acceptor = socket.listen().unwrap();
+    
+    // we can limit the incoming connection count.
+    //for stream in acceptor.incoming().take(10 as uint) {
+    for stream in acceptor.incoming() {
+        println!("Saw connection!");
+        let stream = Cell::new(stream);
+        // Start a task to handle the connection
+        do task::spawn {
+            unsafe {
+                visitor_count += 1;
+            }
+            let mut stream = stream.take();
+            let mut buf = [0, ..500];
+            stream.read(buf);
+            let request_str = str::from_utf8(buf);
+            
+            let req_group : ~[&str]= request_str.splitn_iter(' ', 3).collect();
+            if req_group.len() > 2 {
+                let path = req_group[1];
+                println(fmt!("Request for path: \n%?", path));
+                
+                let file_path = &os::getcwd().push(path.replace("/../", ""));
+                if !os::path_exists(file_path) || os::path_is_dir(file_path) {
+                    println(fmt!("Request received:\n%s", request_str));
+                    let response: ~str = fmt!(
+                        "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
+                         <doctype !html><html><head><title>Hello, Rust!</title>
+                         <style>body { background-color: #111; color: #FFEEAA }
+                                h1 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm red}
+                                h2 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm green}
+                         </style></head>
+                         <body>
+                         <h1>Greetings, Krusty!</h1>
+                         <h2>Visitor count: %u</h2>
+                         </body></html>\r\n", unsafe{visitor_count});
+
+                    stream.write(response.as_bytes());
+                }
+                else {
+                    println(fmt!("serve file: %?", file_path));
+                    match io::read_whole_file(file_path) {
+                        Ok(file_data) => {
+                            stream.write(file_data);
+                        }
+                        Err(err) => {
+                            println(err);
+                        }
+                    }
+                }
+            }
+            println!("connection terminates")
+        }
+    }
 }
