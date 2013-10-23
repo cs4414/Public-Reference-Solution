64f20329c6bbc770f157bc156945cce0c2a5ceae
Weilin <mzweilin@gmail.com>
2013-10-23 14:40:03 -0400

    Ported from Rust v0.7 to Rust v0.8, and fixed a bug on empty command.

--- ps2/gash.rs
+++ ps2/gash.rs
@@ -2,43 +2,51 @@
 // gash.rs
 //
 // Reference solution for PS2
-// Running on Rust 0.7
+// Running on Rust 0.8
+//
+// Special thanks to Kiet Tran for porting code from Rust 0.7 to Rust 0.8.
 //
 // University of Virginia - cs4414 Fall 2013
 // Weilin Xu, Purnam Jantrania, David Evans
-// Version 0.1
+// Version 0.2
+//
 
-use std::{io, run, os, path, uint, libc};
+use std::{io, run, os, path, libc};
 use std::task;
 
+fn get_fd(fpath: &str, mode: &str) -> libc::c_int {
+    #[fixed_stack_segment]; #[inline(never)];
+
+    unsafe {
+        let fpathbuf = fpath.to_c_str().unwrap();
+        let modebuf = mode.to_c_str().unwrap();
+        return libc::fileno(libc::fopen(fpathbuf, modebuf));
+    }
+}
+
+fn exit(status: libc::c_int) {
+    #[fixed_stack_segment]; #[inline(never)];
+    unsafe { libc::exit(status); }
+}
 
 fn handle_cmd(cmd_line: &str, pipe_in: libc::c_int, pipe_out: libc::c_int, pipe_err: libc::c_int) {
     let mut out_fd = pipe_out;
     let mut in_fd = pipe_in;
     let err_fd = pipe_err;
     
-    let mut argv: ~[~str] = cmd_line.split_iter(' ').filter(|&x| x != "").transform(|x| x.to_owned()).collect();
-   
+    let mut argv: ~[~str] =
+        cmd_line.split_iter(' ').filter_map(|x| if x != "" { Some(x.to_owned()) } else { None }).to_owned_vec();
     let mut i = 0;
+    // found problem on redirection
+    // `ping google.com | grep 1 > ping.txt &` didn't work
+    // because grep won't flush the buffer until terminated (only) by SIGINT.
     while (i < argv.len()) {
         if (argv[i] == ~">") {
             argv.remove(i);
-            let fpath = argv.remove(i);
-            unsafe {
-                out_fd = os::as_c_charp(fpath.to_str(), |pathbuf| {
-                            os::as_c_charp("w", |modebuf| 
-                            libc::fileno(libc::fopen(pathbuf, modebuf))
-                    )});
-            }
+            out_fd = get_fd(argv.remove(i), "w");
         } else if (argv[i] == ~"<") {
             argv.remove(i);
-            let fpath = argv.remove(i);
-            unsafe {
-                in_fd = os::as_c_charp(fpath.to_str(), |pathbuf| {
-                            os::as_c_charp("r", |modebuf| 
-                            libc::fileno(libc::fopen(pathbuf, modebuf))
-                    )});
-            }
+            in_fd = get_fd(argv.remove(i), "r");
         }
         i += 1;
     }
@@ -49,8 +57,8 @@ fn handle_cmd(cmd_line: &str, pipe_in: libc::c_int, pipe_out: libc::c_int, pipe_
             ~"help"     => {println("This is a new shell implemented in Rust!")}
             ~"cd"       => {if argv.len()>0 {os::change_dir(&path::PosixPath(argv[0]));}}
             //global variable?
-            //~"history"  => {for uint::range(0, history.len()) |i| {println(fmt!("%5u %s", i+1, history[i]));}}
-            ~"exit"     => {unsafe{libc::exit(0);}}
+            //~"history"  => {for i in range(0, history.len()) {println(fmt!("%5u %s", i+1, history[i]));}}
+            ~"exit"     => {exit(0);}
             _           => {let mut prog = run::Process::new(program, argv, run::ProcessOptions {
                                                                                         env: None,
                                                                                         dir: None,
@@ -71,18 +79,23 @@ fn handle_cmd(cmd_line: &str, pipe_in: libc::c_int, pipe_out: libc::c_int, pipe_
 fn handle_cmdline(cmd_line:&str, bg_flag:bool)
 {
     // handle pipes
-    let progs: ~[~str] = cmd_line.split_str_iter("|").transform(|x| x.to_owned()).collect();
+    let progs: ~[~str] =
+        cmd_line.split_iter('|').filter_map(|x| if x != "" { Some(x.to_owned()) } else { None }).to_owned_vec();
     
-    let mut pipes = ~[];
-    for uint::range(0, progs.len()-1) |_|{
-        pipes.push(os::pipe());
+    let mut pipes: ~[os::Pipe] = ~[];
+    
+    // create pipes
+    if (progs.len() > 1) {
+        for _ in range(0, progs.len()-1) {
+            pipes.push(os::pipe());
+        }
     }
         
     if progs.len() == 1 {
         if bg_flag == false { handle_cmd(progs[0], 0, 1, 2); }
         else {task::spawn_sched(task::SingleThreaded, ||{handle_cmd(progs[0], 0, 1, 2)});}
     } else {
-        for uint::range(0, progs.len()) |i| {
+        for i in range(0, progs.len()) {
             let prog = progs[i].to_owned();
             
             if i == 0 {
@@ -91,14 +104,14 @@ fn handle_cmdline(cmd_line:&str, bg_flag:bool)
             } else if i == progs.len() - 1 {
                 let pipe_i_1 = pipes[i-1];
                 if bg_flag == true {
-                    task::spawn_sched(task::SingleThreaded, ||{handle_cmd(prog, pipe_i_1.in, 1, 2)});
+                    task::spawn_sched(task::SingleThreaded, ||{handle_cmd(prog, pipe_i_1.input, 1, 2)});
                 } else {
-                    handle_cmd(prog, pipe_i_1.in, 1, 2);
+                    handle_cmd(prog, pipe_i_1.input, 1, 2);
                 }
             } else {
                 let pipe_i = pipes[i];
                 let pipe_i_1 = pipes[i-1];
-                task::spawn_sched(task::SingleThreaded, ||{handle_cmd(prog, pipe_i_1.in, pipe_i.out, 2)});
+                task::spawn_sched(task::SingleThreaded, ||{handle_cmd(prog, pipe_i_1.input, pipe_i.out, 2)});
             }
         }
     }
@@ -112,18 +125,20 @@ fn main() {
         print(CMD_PROMPT);
         
         let mut cmd_line = io::stdin().read_line();
-        history.push(copy(cmd_line));
+        cmd_line = cmd_line.trim().to_owned();
+        if cmd_line.len() > 0 {
+            history.push(cmd_line.to_owned());
+        }
         let mut bg_flag = false;
-        let amp_pos = cmd_line.find('&');
-        if amp_pos != None {
-            cmd_line = cmd_line.slice_to(amp_pos.get()).to_owned();
+        if cmd_line.ends_with("&") {
+            cmd_line = cmd_line.trim_right_chars(&'&').to_owned();
             bg_flag = true;
         }
         
         if cmd_line == ~"exit" {
             break;
         } else if cmd_line == ~"history" {
-            for uint::range(0, history.len()) |i| {
+            for i in range(0, history.len()) {
                 println(fmt!("%5u %s", i+1, history[i]));
             }
         } else {
